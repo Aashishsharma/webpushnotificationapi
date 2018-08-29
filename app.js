a4b0586350b179f926484c9468ffca86b1393c94
pujac70 <chauhan.puja777@gmail.com>
2018-08-29 18:04:18 +0530

    Changes for querying mongo db.

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
 var express = require('express');
 var app = express();
-var mongoose = require("mongoose");
+var MongoClient = require("mongodb");
+
 
 const path = require('path');
 const PORT = process.env.PORT || 5000
 
 var MONGOLAB_URI = "mongodb://test:test123@ds233452.mlab.com:33452/subscription-datastore";
-mongoose.connect(MONGOLAB_URI, function (error) {
-    if (error) console.error(error);
-    else console.log('mongo connected');
-});
-
-var keysSchema = new mongoose.Schema ({
-	p256dh: String,
-	auth: String
-})
-
-var saveSubscriptionSchema = new mongoose.Schema({
- endpoint: String,
- expirationTime: String,
- keys: [keysSchema]
-});
-
-var SaveSubscriptionModel = mongoose.model("SaveSubscriptionModel", saveSubscriptionSchema);
 
 app.get('/ashish', function (req, res) {
    res.send('Hello World');
@@ -36,14 +20,26 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+
 app.post('/api/save-subscription/', function (req, res) {
   console.log('Inside save subscription');
-  console.log(req.body.endpoint);
-  SaveSubscriptionModel.findById("5b86650ffb6fc03893e4e567", function ( err, todo ) {
-  	 console.log("ID retrieved from Mongo:"+todo);
-      res.json(200, todo);
-    });
 
+MongoClient.connect(MONGOLAB_URI, function(err, db){
+	if(err){
+		console.log(err);
+	}
+	console.log(db);
+	const myAwesomeDB = db.db('subscription-datastore');
+	var query = {eid : "1234567"};
+	var cursor = myAwesomeDB.collection('Subscription').find(query);
+	cursor.each(function(err, doc){
+		if(doc){
+			console.log(doc);
+		console.log("data found");
+		}
+		
+	});
+});
   res.send(JSON.stringify({ data: { success: true } }));
 });
 
