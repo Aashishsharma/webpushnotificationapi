538c718caa003d856993738290ba75b5fe8167c9
Sourav <sourav.jain3690@gmail.com>
2018-08-29 15:10:01 +0530

    Mongoose configuration added
    Mongoose configuration added

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ var app = express();
 
 const path = require('path');
 const PORT = process.env.PORT || 5000
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;mongoose.connect("mongodb://pujac70:raipur$77@ds233452.mlab.com:33452/subscription-datastore");
+
+var keysSchema = new Schema ({
+	p256dh: String,
+	auth: String
+})
+
+var saveSubscriptionSchema = new mongoose.Schema({
+ endpoint: String,
+ expirationTime: String,
+ keys: [keysSchema]
+});
+
+var SaveSubscriptionModel = mongoose.model("SaveSubscriptionModel", saveSubscriptionSchema);
+
 app.get('/ashish', function (req, res) {
    res.send('Hello World');
 })
@@ -17,10 +33,16 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 
+
+
 app.post('/api/save-subscription/', function (req, res) {
   console.log('Inside save subscription');
   console.log(req.body.endpoint);
- 
+  SaveSubscriptionModel.findById("5b86650ffb6fc03893e4e567", function ( err, todo ) {
+  	 console.log("ID retrieved from Mongo:"+todo);
+      res.json(200, todo);
+    });
+
   res.send(JSON.stringify({ data: { success: true } }));
 });
 
